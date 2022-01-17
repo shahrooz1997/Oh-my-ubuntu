da5590cfba3777d090a67e2d4d08ee4b25692aad
Hamidreza Zare <shahrooz.1000@gmail.com>
2022-01-16 20:38:25 -0500

    Intalling Sublime updated

--- install.sh
+++ install.sh
@@ -5,15 +5,14 @@ sudo ls > /dev/null
 set -e
 
 # Install Apps
-sudo apt-get install -y git tig wget curl unrar unzip vim vlc python python3 ansible gnome-tweaks gparted python-virtualenv proxychains uget python3-pip python-pip
+sudo apt-get install -y git tig wget curl unrar unzip vim vlc python python3 ansible gnome-tweaks gparted python-virtualenv proxychains uget python3-pip python-pip apt-transport-https ca-certificates curl software-properties-common
 git config --global user.name "Hamidreza Zare"
 git config --global user.email "shahrooz.1000@gmail.com"
 sudo update-alternatives --set editor $(which vim.basic)
 
 # Install sublime
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-sudo apt-get install -y apt-transport-https
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+curl -fsSL https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo add-apt-repository "deb https://download.sublimetext.com/ apt/stable/"
 sudo apt-get update
 sudo apt-get install sublime-text
 
