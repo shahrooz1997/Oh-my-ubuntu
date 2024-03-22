55db720eb1eefc0e812a568cca1c7cb76ff2f54b
Hamidreza Zare <shahrooz.1000@gmail.com>
2024-03-22 10:27:21 -0400

    Update the script to make it suitable for Ubuntu 22.04

--- install.sh
+++ install.sh
@@ -5,14 +5,15 @@ sudo ls > /dev/null
 set -e
 
 # Install Apps
-sudo apt-get install -y git tig wget curl unrar unzip vim build-essential vlc python python3 ansible gnome-tweaks gparted python-virtualenv proxychains uget python3-pip python-pip apt-transport-https ca-certificates curl software-properties-common pinta
+sudo apt-get update
+sudo apt-get install -y git tig wget curl unrar unzip vim build-essential vlc python3 ansible gnome-tweaks gparted proxychains uget python3-pip apt-transport-https ca-certificates software-properties-common pinta file gnome-tweaks gcc-12
 git config --global user.name "Hamidreza Zare"
 git config --global user.email "shahrooz.1000@gmail.com"
 sudo update-alternatives --set editor $(which vim.basic)
 
 # Install sublime
-curl -fsSL https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-sudo add-apt-repository "deb https://download.sublimetext.com/ apt/stable/"
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
 sudo apt-get install sublime-text
 
@@ -25,17 +26,16 @@ is_virtual=$(sudo virt-what)
 if [ -z $is_virtual ]; then
 	# Install VirtualBox
 	cd /tmp
-	wget https://download.virtualbox.org/virtualbox/6.1.32/virtualbox-6.1_6.1.32-149290~Ubuntu~bionic_amd64.deb
-	sudo apt-get install -y libcurl4 libqt5core5a libqt5gui5 libqt5opengl5 libqt5printsupport5 libqt5widgets5 libqt5x11extras5 libsdl1.2debian
-	sudo dpkg -i virtualbox-6.1_6.1.32-149290~Ubuntu~bionic_amd64.deb
+	wget https://download.virtualbox.org/virtualbox/7.0.14/virtualbox-7.0_7.0.14-161095~Ubuntu~jammy_amd64.deb
+	sudo apt install -y /tmp/virtualbox-7.0_7.0.14-161095~Ubuntu~jammy_amd64.deb
 
 	# Download VirtualBox Guest Additions
 	cd ~/Downloads/
-	wget https://download.virtualbox.org/virtualbox/6.1.32/VBoxGuestAdditions_6.1.32.iso
+	wget https://download.virtualbox.org/virtualbox/7.0.14/VBoxGuestAdditions_7.0.14.iso
 
-	# Download Ubuntu 18 iso
+	# Download Ubuntu 22 iso
 	cd ~/Downloads/
-	wget https://releases.ubuntu.com/18.04/ubuntu-18.04.6-desktop-amd64.iso
+	wget https://releases.ubuntu.com/22.04/ubuntu-22.04.4-desktop-amd64.iso
 else
 	# Download VirtualBox Guest Additions
 	cd /tmp
@@ -53,12 +53,8 @@ sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O 
 sudo chsh -s $(which zsh) $(whoami)
 
 # Installing Telegram
-cd /tmp
-wget https://updates.tdesktop.com/tlinux/tsetup.3.4.8.tar.xz
-tar -xvf tsetup.3.4.8.tar.xz
-sudo mv Telegram /opt/
-cd
-/opt/Telegram/Telegram &
+sudo snap install telegram-desktop
+telegram-desktop &
 
 # Install fonts
 cd $cur_path
@@ -86,11 +82,11 @@ if [ -f ssh.tar.gz ]; then
 fi
 
 cd /tmp
-wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-357.0.0-linux-x86_64.tar.gz
-tar -xzf google-cloud-sdk-357.0.0-linux-x86_64.tar.gz
+wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-468.0.0-linux-x86_64.tar.gz
+tar -xzf google-cloud-cli-468.0.0-linux-x86_64.tar.gz
 sudo mv google-cloud-sdk /opt/
 cd /opt
-./google-cloud-sdk/install.sh -q --rc-path /home/test/.zshrc
+./google-cloud-sdk/install.sh -q --rc-path /home/$USER/.zshrc
 
 
 gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
@@ -99,63 +95,15 @@ backgroundURI=${cur_path}/background.png
 gsettings set org.gnome.desktop.background picture-uri ${backgroundURI}
 gsettings set org.gnome.desktop.background primary-color '#000000'
 gsettings set org.gnome.desktop.background picture-options 'scaled'
-gsettings set org.gnome.nautilus.desktop volumes-visible false
 gsettings set org.gnome.desktop.wm.preferences button-layout ':minimize,close'
 gsettings set org.gnome.desktop.interface clock-show-date true
 gsettings set org.gnome.desktop.interface font-name 'Ubuntu 14'
 gsettings set org.gnome.desktop.interface document-font-name 'Source Code Pro 15'
 gsettings set org.gnome.desktop.wm.preferences titlebar-font 'Ubuntu 15'
 
-cd /tmp
-wget https://download-cdn.jetbrains.com/cpp/CLion-2021.2.2.tar.gz
-tar -xzf CLion-2021.2.2.tar.gz
-sudo mv clion-2021.2.2 /opt/
-cd /opt/clion-2021.2.2/bin
-./clion.sh &
-sudo ln -s /opt/clion-2021.2.2/bin/clion.sh /usr/local/bin/clion
-sudo bash -c 'printf "[Desktop Entry]
-Name=CLion
-Exec=/opt/clion-2021.2.2/bin/clion.sh
-Icon=/opt/clion-2021.2.2/bin/clion.png
-Terminal=false
-Type=Application
-Encoding=UTF-8
-Categories=Application;
-Name[en_US]=CLion\n" > /usr/share/applications/CLion.desktop'
-
-cd /tmp
-wget https://download-cdn.jetbrains.com/python/pycharm-professional-2021.2.2.tar.gz
-tar -xzf pycharm-professional-2021.2.2.tar.gz
-sudo mv pycharm-2021.2.2 /opt/
-sudo ln -s /opt/pycharm-2021.2.2/bin/pycharm.sh /usr/local/bin/pycharm
-sudo bash -c 'printf "[Desktop Entry]
-Name=PyCharm
-Exec=/opt/pycharm-2021.2.2/bin/pycharm.sh
-Icon=/opt/pycharm-2021.2.2/bin/pycharm.png
-Terminal=false
-Type=Application
-Encoding=UTF-8
-Categories=Application;
-Name[en_US]=PyCharm\n" > /usr/share/applications/PyCharm.desktop'
-
-cd /tmp
-wget https://download-cdn.jetbrains.com/idea/ideaIU-2021.2.2.tar.gz
-tar -xzf ideaIU-2021.2.2.tar.gz
-sudo mv idea-IU-212.5284.40 /opt/
-sudo ln -s /opt/idea-IU-212.5284.40/bin/idea.sh /usr/local/bin/idea
-sudo bash -c 'printf "[Desktop Entry]
-Name=Idea
-Exec=/opt/idea-IU-212.5284.40/bin/idea.sh
-Icon=/opt/idea-IU-212.5284.40/bin/idea.png
-Terminal=false
-Type=Application
-Encoding=UTF-8
-Categories=Application;
-Name[en_US]=Idea\n" > /usr/share/applications/Idea.desktop'
-
-# Install VS Code
-cd /tmp
-wget https://az764295.vo.msecnd.net/stable/83bd43bc519d15e50c4272c6cf5c1479df196a4d/code_1.60.1-1631294805_amd64.deb
-sudo dpkg -i code_1.60.1-1631294805_amd64.deb
-
-
+sudo snap install --classic code
+sudo snap install --classic intellij-idea-ultimate
+sudo snap install --classic clion
+sudo snap install --classic pycharm-professional
+sudo snap install --classic datagrip
+sudo snap install --classic webstorm
